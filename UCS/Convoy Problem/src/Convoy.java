836b9775dcdae4df4e9cbd236f65d2b3f3349692
LostWell <lmdl.el2@gmail.com>
2018-09-24 16:34:52 +0800

    Semi-Final iteration of program

--- UCS/Convoy Problem/src/Convoy.java
+++ UCS/Convoy Problem/src/Convoy.java
@@ -1,7 +1,6 @@
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
-import java.util.Comparator;
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
@@ -16,14 +15,66 @@ public class Convoy {
 			int totalVehicles = reader.nextInt();
 			ArrayList<Truck> vehicleList = new ArrayList<Truck>();
 			
-			while(reader.hasNextLine()) {
+			for(int i = 1; reader.hasNextLine() && i <= totalVehicles && i <= 1000; i++) {
 				vehicleList.add(new Truck(reader.nextInt(), reader.nextInt()));
 			}
 			reader.close();
 			
-			State initialState = new State(vehicleList, null, 0, 0, 0, null);
+			PriorityQueue<State> frontier = new PriorityQueue<State>();
+			State initialState = new State(vehicleList, new ArrayList<Truck>(), 0, 0, 0, null);
+			frontier.add(initialState);
+			int maxFrontierSize = 1;
+			ArrayList<State> goalStates = new ArrayList<State>();
+			
+			while (frontier.size() > 0){
+				State currentState = frontier.poll();
+				if(currentState.isGoal()) {
+					goalStates.add(currentState);
+				} else {
+					ArrayList<State> successorStates = currentState.expand(maxLoad, bridgeLength);
+					if(successorStates != null) {
+						frontier.addAll(successorStates);
+		                maxFrontierSize = Math.max(maxFrontierSize, frontier.size());
+					}
+				}
+			}
+			
+			State solution = goalStates.get(0);
+			if (goalStates != null) {
+				for(int i = 0; i < goalStates.size(); i++) {
+					if(goalStates.get(i).getPathTime() <= solution.getPathTime()) {
+						solution = goalStates.get(i);
+					}
+				}
+			}
+			showSolution(solution, maxLoad, bridgeLength, totalVehicles, maxFrontierSize);
 		} catch(FileNotFoundException e) {
 			System.out.println("File not Found");
+		}
+	}
+	
+	public static void showSolution(State solution, int maxLoad, int bridgeLength, int totalVehicles, int maxFrontierSize) {
+		if(solution == null) {
+			System.out.println("There is no solution");
+		} else {
+			ArrayList<State> path = new ArrayList<State>();
+			State currentState = solution;
+			while (currentState != null) {
+				path.add(0, currentState);
+				currentState = currentState.getParent();
+			}
+			
+			System.out.println("-----------Solution-----------");
+			System.out.printf("Minimum Time Utilized: %.1f%n", solution.getPathTime());
+			System.out.println("Maximum Bridge Load: " + maxLoad);
+			System.out.println("Bridge Length: " + bridgeLength);
+			System.out.println("Number of Vehicles: " + totalVehicles);
+			System.out.println("Maximum Frontier Size: " + maxFrontierSize);
+			
+			for(State st : path) {
+				System.out.println(st.toString());
+			}
+			
 		}
 	}
 }
--- UCS/Convoy Problem/src/State.java
+++ UCS/Convoy Problem/src/State.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class State implements Comparable<State>{
 	private ArrayList<Truck> vehiclesLeft, vehiclesPassed;
-	private int pathTime, batchTime, batchWeight;
+	private float pathTime, batchTime;
+	private int batchWeight;
 	private State parent;
 	
-    public State(ArrayList<Truck> vehiclesLeft, ArrayList<Truck> vehiclesPassed, int pathTime, int batchTime, int batchWeight, State parent) {
+    public State(ArrayList<Truck> vehiclesLeft, ArrayList<Truck> vehiclesPassed, float pathTime, float batchTime, int batchWeight, State parent) {
     	this.vehiclesLeft = vehiclesLeft;
     	this.vehiclesPassed = vehiclesPassed;
     	this.pathTime = pathTime;
@@ -30,19 +32,19 @@ public class State implements Comparable<State>{
 		this.vehiclesPassed = trucksPassed;
 	}
 
-	public int getPathTime() {
+	public float getPathTime() {
 		return pathTime;
 	}
 
-	public void setPathTime(int pathTime) {
+	public void setPathTime(float pathTime) {
 		this.pathTime = pathTime;
 	}
 	
-	public int getBatchTime() {
+	public float getBatchTime() {
 		return batchTime;
 	}
 
-	public void setBatchTime(int batchSpeed) {
+	public void setBatchTime(float batchSpeed) {
 		this.batchTime = batchSpeed;
 	}
 
@@ -63,33 +65,50 @@ public class State implements Comparable<State>{
 	}
 	
     public int compareTo(State other) {
-        return this.getBatchTime() - other.getBatchTime();
+        return Math.round(this.getBatchTime()) - Math.round(other.getBatchTime());
     }
     
     public ArrayList<State> expand(int maxLoad, int bridgeLength){
     	ArrayList<State> successorStates = new ArrayList<State>();
-    	for(int x = 0; x < this.vehiclesLeft.size() && weightChecker(x) <= maxLoad; x++) {
-        	State successor = this;
-        	int slowestSpeed = 0;
-        	int totalBatchWeight = 0;
+    	if(this.getVehiclesLeft().size()!=0) {
+    		
+    		ArrayList<Truck> left = new ArrayList<Truck>();
+    		left.addAll(this.getVehiclesLeft());
+    		ArrayList<Truck> passed = new ArrayList<Truck>();
+    		passed.addAll(this.getVehiclesPassed());
+        	State parent = new State(left, passed, this.getPathTime(), this.getBatchTime(), this.getBatchWeight(), this.getParent());
         	
-    		for(int y = 0; y <= x; y++) {
-    			successor.getVehiclesPassed().add(successor.getVehiclesLeft().get(0));
-    			totalBatchWeight += successor.getVehiclesLeft().get(0).getWeight();
-    			if (slowestSpeed < successor.getVehiclesLeft().get(0).getSpeed()) {
-    				slowestSpeed = successor.getVehiclesLeft().get(0).getSpeed();
-    			}
-    			successor.getVehiclesLeft().remove(0);
-    		}
-    		
-    		successor.setBatchTime((bridgeLength / slowestSpeed)*60);
-    		successor.setBatchWeight(totalBatchWeight);
-    		successor.setPathTime(this.getPathTime() + successor.getBatchTime());
-    		successor.setParent(this);
-    		
-    		successorStates.add(successor);
+    		for(int x = 0; x < parent.vehiclesLeft.size() && weightChecker(x) <= maxLoad; x++) {
+    			
+        		ArrayList<Truck> left1 = new ArrayList<Truck>();
+        		left1.addAll(parent.getVehiclesLeft());
+        		ArrayList<Truck> passed1 = new ArrayList<Truck>();
+        		passed1.addAll(parent.getVehiclesPassed());
+            	State successor = new State(left1, passed1, parent.getPathTime(), parent.getBatchTime(), parent.getBatchWeight(), parent.getParent());
+            	
+            	int slowestSpeed = successor.getVehiclesLeft().get(0).getSpeed();
+            	int totalBatchWeight = 0;
+            	
+        		for(int y = 0; y <= x; y++) {
+        			successor.getVehiclesPassed().add(successor.getVehiclesLeft().get(0));
+        			totalBatchWeight += successor.getVehiclesLeft().get(0).getWeight();
+        			if (slowestSpeed > successor.getVehiclesLeft().get(0).getSpeed()) {
+        				slowestSpeed = successor.getVehiclesLeft().get(0).getSpeed();
+        			}
+        			successor.getVehiclesLeft().remove(0);
+        		}
+        		
+        		successor.setBatchTime(((float)bridgeLength / slowestSpeed)*60);
+        		successor.setBatchWeight(totalBatchWeight);
+        		successor.setPathTime(parent.getPathTime() + successor.getBatchTime());
+        		successor.setParent(parent);
+        		
+        		successorStates.add(successor);
+        	}
+        	return successorStates;
+    	} else {
+    		return null;
     	}
-    	return successorStates;
     }
     
     public int weightChecker(int limit) {
@@ -99,4 +118,17 @@ public class State implements Comparable<State>{
     	}
     	return weight;
     }
+    
+	public boolean isGoal() {
+		if(this.getVehiclesLeft().isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return ("------------------------------\n" + "Vehicles Passed: " + this.vehiclesPassed.size() + ";\n" + "Vehicles Left: " + this.vehiclesLeft.size() + ";\n" + "Batch Weight: " + this.batchWeight + ";\n" + "Batch Time: " + this.batchTime + ";\n" + "Total Path Time: " + this.pathTime);
+	}
 }
